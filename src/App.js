21111ef255cd1dd9d5411ef21736b51aad2ea419
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-06-03 23:12:07 +0200

    Added search box

--- src/App.js
+++ src/App.js
@@ -11,11 +11,19 @@ export default function App() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => setMonsters(users))
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   return (
     <div className="App">
+      <input
+        className="search-box"
+        type="search"
+        placeholder="search monsters"
+        onChange={(event) => console.log(event)}
+      />
       {monsters.map((monster) => (
         <h1 key={monster.name}>{monster.name}</h1>
       ))}
@@ -51,6 +59,11 @@ export default function App() {
 //   render() {
 //     return (
 //       <div className="App">
+//         <input
+//           className="search-box"
+//           type="search"
+//           placeholder = "search monsters"
+//           onChange={(event) => { console.log(event) }} />
 //         {this.state.monsters.map((monster) => {
 //           return <h1>{monster.name}</h1>;
 //         })}
