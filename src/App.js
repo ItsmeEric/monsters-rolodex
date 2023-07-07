eb0b786b3865d9f33f560f6cbdc228cd7501b808
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-07-07 21:17:38 +0200

    Added font

--- src/App.js
+++ src/App.js
@@ -82,6 +82,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox
           className="monsters-search-Box"
           onChangeHandler={onSearchChange}
