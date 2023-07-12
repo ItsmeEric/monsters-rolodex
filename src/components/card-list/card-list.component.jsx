0ac64390fc97d760a8efad97dadd577ac92f7aa8
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-07-12 23:18:11 +0200

    Cardlist into function component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,15 +2,13 @@ import Card from "../card/card.component";
 import './card-list.styles.css'
 
 // Destructure out monster props
-const CardList = ({ monsters }) => {
-    (
-        <div className="card-list">
-            {monsters.map((monster) => {
-                return (
-                    <Card monster={monster} />
-                )})}
-        </div>
-    )
-}
+const CardList = ({ monsters }) => (
+    <div className="card-list">
+        {monsters.map((monster) => {
+            return (
+                <Card monster={monster} />
+            )})}
+    </div>
+)
 
 export default CardList;
